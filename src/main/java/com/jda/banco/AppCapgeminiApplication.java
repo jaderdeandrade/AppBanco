5467bfb353facac327bd39b221504ac989743d84
jaderdeandrade <jaderdeandrade@gmail.com>
2019-07-31 10:22:10 -0300

    Projeto criado

---
+++ src/main/java/com/jda/banco/AppCapgeminiApplication.java
@@ -0,0 +1,13 @@
+package com.jda.banco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCapgeminiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppCapgeminiApplication.class, args);
+	}
+
+}
